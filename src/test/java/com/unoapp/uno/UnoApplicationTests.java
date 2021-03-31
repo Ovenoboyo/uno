933fad295fc067075421fa6d8c34a974969e1e28
ovenoboyo <ovenoboyo@gmail.com>
2021-03-31 11:57:04 +0530

    Initial commit

---
+++ src/test/java/com/unoapp/uno/UnoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unoapp.uno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
